6c64532a37db16f56242efa3b2ee298ad70c4ffc
jkzzk <1745493589@qq.com>
2020-06-02 10:58:15 +0800

    'maven相关知识'

---
+++ maven_lesson_1/src/main/java/com/jkzzk/maven/Calculate.java
@@ -0,0 +1,21 @@
+package com.jkzzk.maven;
+
+/**
+ * @describe 计算类，用于一些常用的整形计算
+ * @author jkzzk
+ * @date 2020年6月2日
+ */
+public class Calculate {
+
+    /**
+     * @describe 计算两个数之和
+     * @param add_1 int类型数据
+     * @param add_2 int类型数据
+     * @return 参数之和，int类型数据
+     * @author jkzzk
+     * @date 2020年6月2日
+     */
+    public static int sum(int add_1,int add_2) {
+        return add_1 + add_2;
+    }
+}
---
+++ maven_lesson_1/src/test/java/com/jkzzk/maven/test/TestCalculate.java
@@ -0,0 +1,13 @@
+package com.jkzzk.maven.test;
+
+import com.jkzzk.maven.Calculate;
+import org.junit.Test;
+
+public class TestCalculate {
+
+    @Test
+    public void testSum() {
+        System.out.println(Calculate.sum(1,2));
+    }
+
+}
